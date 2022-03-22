5ed76ec450fae690e256927c9b8c67cc0144cb78
Sergio Mancilla <sdmancilla@uninorte.edu.co>
2022-03-21 19:17:30 -0500

    styles for settings added

--- src/webapp/public/scripts/settings.js
+++ src/webapp/public/scripts/settings.js
@@ -3,8 +3,10 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     const question = document.getElementById("question")
     const answers = Array.from(event.target.querySelectorAll("input[type='text']"));
-    const corrects = Array.from(event.target.querySelectorAll(".correct"));
+    const radioOptions = Array.from(event.target.querySelectorAll("input[type='radio']"))
+    const corrects = Array.from(event.target.querySelectorAll(".option"));
     const difficulties = Array.from(event.target.querySelectorAll(".difficulty"));
+    const success = document.getElementById("success")
 
     const correct = corrects.findIndex(elem => elem.checked == true)
     const difficulty = difficulties.find(elem => elem.checked == true)
@@ -19,4 +21,11 @@ form.addEventListener("submit", (event) => {
         body: JSON.stringify(body),
         headers: { "Content-Type": "application/json; charset=utf-8" }
     })
+    answers.map(answer => answer.value = "")
+    radioOptions.map(radio => radio.checked = false)
+    success.classList.add("visible")
+    setTimeout(() => {
+        success.classList.remove("visible")
+    }, 1000)
+    
 });
