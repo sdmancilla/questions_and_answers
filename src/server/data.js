7ae8b222dbbe856e7416a9c6e4089c4b566d8532
Sergio Mancilla <sdmancilla@uninorte.edu.co>
2022-03-21 20:23:45 -0500

    last commit

--- src/server/data.js
+++ src/server/data.js
@@ -1,36 +1,31 @@
 const questions = [
     {
-        question: '¿Cuál es el color del agua?',
-        answers: ['Azul', 'Blanco', 'Amarillo', 'Verde'],
-        correct: 3,
+        question: '¿Cuántos planetas tiene el sistema solar?',
+        answers: ['5', '7', '8', '9'],
+        correct: 2,
         difficulty: 'begginer'
     },
     {
-        question: '¿Cuántos pelos tiene un gato?',
-        answers: ['Muchos', 'Pocos', '3', 'Más'],
+        question: '¿Qué elemento representa Na en la tabla periódica?',
+        answers: ['Sodio', 'Potasio', 'Nadio', 'Berilio'],
         correct: 0,
         difficulty: 'easy'
     },
     {
-        question: '¿Cuántos gatos caben en un cajón de 3 posiciones?',
-        answers: ['1', '2', '3', '4'],
+        question: '¿Cuál es la raíz cuadrada de 169?',
+        answers: ['11', '13', '9', '14'],
         correct: 1,
         difficulty: 'medium'
     },
     {
-        question: '¿A qué edad se pensiona Capacho?',
-        answers: ['68', '75', '30', 'Nunca'],
+        question: '¿Cuál es el color característico de las amatistas?',
+        answers: ['Azul', 'Verde', 'Rojo', 'Violeta'],
         correct: 3,
         difficulty: 'hard'
     },
     {
-        question: 'Complete la oración: Soy',
-        answers: [
-            'Una buena persona',
-            'Esta no es',
-            'Esta tampoco',
-            'Esta menos'
-        ],
+        question: '¿Quién mató a Hittler?',
+        answers: ['Hittler', 'Un guardia de Napoleón Bonaparte', 'Un soldado alemán', 'Un soldado polaco'],
         correct: 0,
         difficulty: 'impossible'
     }
